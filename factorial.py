b04f91c7460ee06b5342b1b7dd6502deedfc2afb
Aashish Fartyal <112853824+kiyo124@users.noreply.github.com>
2025-07-01 15:07:04 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,8 @@
+a=int(input("enter number to find factorial:"))
+def fact(b):
+    if b<2:
+        return 1
+    else:
+        return b*(fact(b-1))
+fa=fact(a)
+print("factorial of number is: ",fa)
